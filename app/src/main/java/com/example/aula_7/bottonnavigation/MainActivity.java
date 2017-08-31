ea394d6784076d85dd21ff4ef53f42ac7b068863
Josephrosero <Josephrosero47@gmail.com>
2017-08-31 13:46:26 -0500

    Final asta el 2

--- app/src/main/java/com/example/aula_7/bottonnavigation/MainActivity.java
+++ app/src/main/java/com/example/aula_7/bottonnavigation/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenu;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -25,10 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
                    case R.id.item_1:
                        Toast.makeText(MainActivity.this,"Item 1", Toast.LENGTH_SHORT).show();
-                       break;
+                       UnoFragment pantalla1 = new UnoFragment();
+                       getSupportFragmentManager()
+                               .beginTransaction()
+                               .replace(R.id.frl_1, pantalla1)
+                               .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                               .addToBackStack(null)
+                               .commit();                       break;
 
                    case R.id.item_2:
                        Toast.makeText(MainActivity.this,"Item 2", Toast.LENGTH_SHORT).show();
+
+                       DosFragment pantalla2 = new DosFragment();
+                       getSupportFragmentManager()
+                               .beginTransaction()
+                               .replace(R.id.frl_1, pantalla2)
+                               .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                               .addToBackStack(null)
+                               .commit();
                        break;
 
                    case R.id.item_3:
